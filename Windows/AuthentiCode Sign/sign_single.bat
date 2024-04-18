895ed0ae564019eca8ba3a08a7b39073dcd09dc5
Daniel Marschall <info@daniel-marschall.de>
2024-04-18 22:57:48 +0000

    sign_single.bat update
    git-svn-id: https://svn.viathinksoft.com/svn/checksum-tools/trunk@24 b4ddfd75-dbbf-4721-b1f5-0ed84741e47a

--- Windows/AuthentiCode Sign/sign_single.bat
+++ Windows/AuthentiCode Sign/sign_single.bat
@@ -4,12 +4,22 @@ if "%~1"=="" goto :done
 
 echo Sign %1 ...
 
-echo "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22000.0\x64\signtool.exe" verify /pa %1
 >NUL 2>&1 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22000.0\x64\signtool.exe" verify /pa %1 && (
-  echo File %2 is already signed, skipping
+	echo File %1 is already signed, skipping
+	exit /b 0
 ) || (
-  "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22000.0\x64\signtool.exe" sign /a /tr http://timestamp.globalsign.com/tsa/r6advanced1 /td SHA256 /fd SHA256 /n "HickelSOFT" /v %1
-  exit /b 0
+	"C:\Program Files (x86)\Windows Kits\10\bin\10.0.22000.0\x64\signtool.exe" sign /a /tr http://timestamp.globalsign.com/tsa/r6advanced1 /td SHA256 /fd SHA256 /n "HickelSOFT" /v %1
+	>NUL 2>&1 "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22000.0\x64\signtool.exe" verify /pa %1 && (
+		echo File %1 is sucessfully signed!
+		exit /b 0
+	) || (
+		echo ERROR while signing file %1 !
+		echo.
+		"C:\Program Files (x86)\Windows Kits\10\bin\10.0.22000.0\x64\signtool.exe" sign /a /tr http://timestamp.globalsign.com/tsa/r6advanced1 /td SHA256 /fd SHA256 /n "HickelSOFT" /v %1
+		echo.
+		pause.
+		exit /b 1
+	)
 )
 
 :done
